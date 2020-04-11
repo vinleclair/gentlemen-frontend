0dc3b176abd93bb10b45d4f38e5ac7981a6e470b
vinleclair <vinleclair@gmail.com>
2020-04-11 10:44:14 -0400

    Remove unused appointment actions

--- src/store/appointment.module.js
+++ src/store/appointment.module.js
@@ -4,11 +4,7 @@ import {
 } from "@/common/api.service";
 import {
     APPOINTMENT_SCHEDULE,
-    APPOINTMENT_DELETE,
-    APPOINTMENT_EDIT,
     APPOINTMENT_RESET_STATE,
-    FETCH_APPOINTMENT
-
 } from "./actions.type";
 import {
     SET_APPOINTMENT,
@@ -19,31 +15,14 @@ const initialState = {
     appointment: {
         name: "",
         email: "",
-        //TODO: add date field
-        //TODO: add barber field
-        //TODO: add haircut type field
     }
 }
 
 export const state = { ...initialState };
 
 export const actions = {
-    async [FETCH_APPOINTMENT](context, appointmentSlug, prevAppointment) {
-        if (prevAppointment !== undefined) {
-            return context.commit(SET_APPOINTMENT, prevAppointment);
-        }
-        const { data } = await AppointmentsService.get(appointmentSlug);
-        context.commit(SET_APPOINTMENT, data.appointment);
-        return data;
-    },
     [APPOINTMENT_SCHEDULE]({ state }) {
         return AppointmentsService.create(state.appointment);
-    },
-    [APPOINTMENT_DELETE](context, slug) {
-        return AppointmentsService.destroy(slug);
-    },
-    [APPOINTMENT_EDIT]({ state }) {
-        return AppointmentsService.update(state.appointment.slug, state.appointment);
     },
     [APPOINTMENT_RESET_STATE]({ commit }) {
         commit(RESET_STATE);
