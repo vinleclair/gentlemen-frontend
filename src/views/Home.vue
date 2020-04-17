81a8383aa0fc5c051893eddd311cf69db3c54602
vinleclair <vinleclair@gmail.com>
2020-04-16 20:56:16 -0400

    Add subtitle and divs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -96,7 +96,21 @@
         </section>
 
         <section id="appointment-booker">
+            <div class="py-12"></div>
+                <v-container class="text-center">
+                    <h2 class="display-2 font-weight-bold mb-3">BOOK APPOINTMENT</h2>
+
+                    <v-responsive
+                            class="mx-auto mb-8"
+                            width="56"
+                    >
+                        <v-divider class="mb-1"></v-divider>
+
+                        <v-divider></v-divider>
+                    </v-responsive>
+                </v-container>
             <AppointmentBooker/>
+            <div class="py-12"></div>
         </section>
 
         <v-sheet
