2db9f1f960a4303693181860168db32caa32f202
vinleclair <vinleclair@gmail.com>
2020-04-17 19:18:26 -0400

    Update to use Heroku

---
+++ dist/precache-manifest.663bea543c37ad576d78e6fb8438d8ab.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f818abf9201aa2646d53",
+    "url": "/css/chunk-40992d84.7b8dd3e7.css"
+  },
+  {
+    "revision": "eacc6ebb954f23bd7667",
+    "url": "/css/chunk-vendors.0778394f.css"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/fonts/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/fonts/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/fonts/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "da4ea5cdfca6b3baab285741f5ccb59f",
+    "url": "/fonts/MaterialIcons-Regular.da4ea5cd.ttf"
+  },
+  {
+    "revision": "84d9ada4ae2928d9cd7308c95bacb32b",
+    "url": "/img/Fredrick.png"
+  },
+  {
+    "revision": "b3ce94109e1b06817204315eb4f7fc6a",
+    "url": "/img/Matthew.png"
+  },
+  {
+    "revision": "6f3f988f5a15d6b885ecbb573b424082",
+    "url": "/img/bg.6f3f988f.png"
+  },
+  {
+    "revision": "1b563c6e22ee75f15774cc9922dddfe2",
+    "url": "/img/haircut.png"
+  },
+  {
+    "revision": "e38dcb217fe16da37184bdbb27a3054b",
+    "url": "/img/logo.e38dcb21.png"
+  },
+  {
+    "revision": "6b6755eddba9aa29e53bd0c29cdb63c0",
+    "url": "/img/parallax.6b6755ed.png"
+  },
+  {
+    "revision": "08c6df285cee2b6f60825c4ba304d3d0",
+    "url": "/img/shave.png"
+  },
+  {
+    "revision": "1a45dd59cfaec108c5850c568c9fa20f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca4371f4373e1f4166d0",
+    "url": "/js/app.10570b88.js"
+  },
+  {
+    "revision": "f818abf9201aa2646d53",
+    "url": "/js/chunk-40992d84.ad84618e.js"
+  },
+  {
+    "revision": "eacc6ebb954f23bd7667",
+    "url": "/js/chunk-vendors.1402c424.js"
+  },
+  {
+    "revision": "1a2f023fdbf616d214baeebf87704cd8",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
