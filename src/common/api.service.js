e2d1aeef01d4170e045ae39240fc5066d4311155
vinleclair <vinleclair@gmail.com>
2020-04-17 15:17:26 -0400

    Remove unused axios requests

--- src/common/api.service.js
+++ src/common/api.service.js
@@ -9,12 +9,6 @@ const ApiService = {
         Vue.axios.defaults.baseURL = API_URL;
     },
 
-    query(resource, params) {
-        return Vue.axios.get(resource, params).catch(error => {
-            throw new Error(`[GM] ApiService ${error}`);
-        });
-    },
-
     get(resource, slug = "") {
         return Vue.axios.get(`${resource}/${slug}`).catch(error => {
             throw new Error(`[GM] ApiService ${error}`);
@@ -24,20 +18,6 @@ const ApiService = {
     post(resource, params) {
         return Vue.axios.post(`${resource}`, params);
     },
-
-    update(resource, slug, params) {
-        return Vue.axios.put(`${resource}/${slug}`, params);
-    },
-
-    put(resource, params) {
-        return Vue.axios.put(`${resource}`, params);
-    },
-
-    delete(resource) {
-        return Vue.axios.delete(resource).catch(error => {
-            throw new Error(`[GM] ApiService ${error}`);
-        });
-    }
 };
 
 export default ApiService;
